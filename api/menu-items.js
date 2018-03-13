5ed0e7ad780b34814b7fb11dd4f39b4a7c0c9337
Kim Paulissen <kim.paulissen@kuleuven.be>
2018-03-13 23:13:40 +0100

    add param, get and delete for menu items

--- api/menu-items.js
+++ api/menu-items.js
@@ -3,6 +3,41 @@ const menuItemsRouter = express.Router({mergeParams: true});
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+menuItemsRouter.param('menuItemId', (req, res, next, menuItemId) => {
+    db.get(`SELECT * FROM MenuItem WHERE id = ${menuItemId}`,
+        (err, data) => {
+            if (err) {
+                next(err);
+            } else if (data) {
+                next();
+            } else {
+                res.sendStatus(404);
+            }
+        });
+});
+
+menuItemsRouter.get('/', (req, res, next) => {
+    db.all(`SELECT * FROM MenuItem WHERE menu_id = ${req.params.menuId}`,
+        (err, data) => {
+            if (err) {
+                next(err);
+            } else {
+                res.status(200).json({menuItems: data});
+            }
+        });
+});
+
+menuItemsRouter.delete('/:menuItemId', (req, res, next) => {
+    db.run(`DELETE FROM MenuItem WHERE id = ${req.params.menuItemId}`,
+        (err) => {
+            if (err) {
+                next(err);
+            } else {
+                res.sendStatus(204);
+            }
+        });
+});
+
 
 
 module.exports = menuItemsRouter;
